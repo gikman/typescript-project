47737d777f918b5b9f715f388f8902c2db932692
gikman <72310618+gikman@users.noreply.github.com>
2020-11-17 11:27:36 +0300

    Add files via upload

---
+++ public/app.js
@@ -0,0 +1,21 @@
+import { Invoice } from './classes/Invoices.js';
+import { ListTemplate } from './classes/ListTemplate.js';
+import { Payment } from './classes/Payment.js';
+const form = document.querySelector('.new-item-form');
+const type = document.querySelector('#type');
+const tofrom = document.querySelector('#tOFROM');
+const details = document.querySelector('#details');
+const amount = document.querySelector('#amount');
+const ul = document.querySelector('ul');
+const list = new ListTemplate(ul);
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
+    let doc;
+    if (type.value === 'invoice') {
+        doc = new Invoice(tofrom.value, details.value, amount.valueAsNumber);
+    }
+    else {
+        doc = new Payment(tofrom.value, details.value, amount.valueAsNumber);
+    }
+    list.render(doc, type.value, 'end');
+});
